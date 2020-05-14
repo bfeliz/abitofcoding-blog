ca2174dd833f34b5190442dcad15b6f21fe57436
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-05-14 10:46:38 -0700

    Setup search element to prep for adding functionality later

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
+    const search = $("#search-area");
+    const searchResults = $("#search-input");
     $(".sidenav").sidenav();
+
+    search.on("submit", function () {
+        console.log(searchResults.val());
+    });
 });
