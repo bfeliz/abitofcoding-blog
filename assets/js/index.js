648de8a04eaa8a61f2a8cad3b581b85ada12d644
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-05-19 16:23:39 -0700

    Add functionality to search bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
     const search = $("#search-area");
     const searchResults = $("#search-input");
+    const searchBar = $("#search-bar");
+    const searchItems = $("#search-results");
+
     $(".sidenav").sidenav();
 
     search.on("submit", function () {
         console.log(searchResults.val());
+    });
+
+    $(".modal").modal({
+        onOpenEnd: function () {
+            $("#search-bar").focus();
+        },
+        onCloseEnd: function () {
+            searchBar.val(" ");
+            searchItems.html("");
+        },
     });
 });
