4237bc5dfdd267bda0340fc042702e8712f8c000
Brittany Fortner <brittany.e.fortner@gmail.com>
2021-02-01 16:00:52 -0700

    Fix card height bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,4 +16,34 @@ $(document).ready(function () {
             searchItems.html("");
         },
     });
+    function sameHeights(selector) {
+        $(selector).css("height", "fit-content");
+        let sel = selector;
+        const query = $(sel);
+        let elements = query.length;
+        let max = 0;
+        if (elements) {
+            while (elements--) {
+                let element = query[elements];
+                console.log("height", element.clientHeight);
+                if (element.clientHeight > max) {
+                    max = element.clientHeight;
+                }
+            }
+            elements = query.length;
+            while (elements--) {
+                let element = query[elements];
+                $(element).css("height", max + "px");
+            }
+        }
+    }
+
+    // event listener for column height match
+    $(window).on("resize", function () {
+        sameHeights($(".card-col"));
+    });
+
+    $(window).on("load", function () {
+        sameHeights($(".card-col"));
+    });
 });
